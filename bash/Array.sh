d2e3860a6f79d4b12dbaab23407ab3baeb76abb3
abhishek <abhishekprasad7250@gmail.com>
2025-01-21 20:17:15 +0530

    arrays

--- bash/Array.sh
+++ bash/Array.sh
@@ -1,2 +1,11 @@
-myArray=(1 2 3 " hello" "world")
-echo "${myArray[1]}"
+# myArray=(1 2 3 4 " hello" "world")
+# myArray+=(5 6 8)
+# echo "${myArray[1]}"
+# echo "${myArray[@]}"  # print all elements of array
+# echo "${#myArray[*]}" # print length of array
+# echo "${myArray[*]:0}" # print all elements except first
+# echo "${myArray[*]:2:1}" # print elements from 1 to 2
+
+myArray=( [name]=Paul [age]=20 )
+
+echo "${myArray[name]}"
