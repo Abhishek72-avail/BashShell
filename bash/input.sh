f9f23855bc83e53bfe60ddf5210c01ac103c3a16
abhishek <abhishekprasad7250@gmail.com>
2025-01-23 23:38:39 +0530

    input code

--- bash/input.sh
+++ bash/input.sh
@@ -2,8 +2,13 @@
 # echo "You entered: $var_name"
 
 
-read -p "Your name: " NAME
-echo "Hello, $NAME!"
+# read -p "Your name: " NAME
+# echo "Hello, $NAME!"
 
 let a=5+4
 echo $a
+
+((a=5*10))
+echo $a #Output: 50
+
+echo $((5 * (3 + 2)))
