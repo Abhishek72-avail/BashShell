959175d6b279008fa271d67f3100669c8f0643e8
abhishek <abhishekprasad7250@gmail.com>
2025-01-23 23:22:52 +0530

    update code

--- bash/day1.sh
+++ bash/day1.sh
@@ -1,3 +1,15 @@
 myArray+=( [city]=NewYork )
 str="Shell Scripting"
 echo ${#str}
+echo ${str/Scripting/Programming} 
+echo ${str:6:10}
+
+myVar="Hello World!"
+length=${#myVar}
+echo $length
+
+upper=${myVar^^}
+echo $upper # HELLO WORLD!
+
+lower=${myVar,,}
+echo $lower # hello world!
