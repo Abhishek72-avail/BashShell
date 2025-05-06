02bd18d7cbbc821c25e157c7d99e6cdd013a91cd
abhishek <abhishekprasad7250@gmail.com>
2025-05-06 11:33:25 +0530

    push all code

---
+++ java/main.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.print(name);
+        // System.out.println(ans);
+    }
+}
